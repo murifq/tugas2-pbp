c7aee8d6c606b54b0b0bfe70004e700ae9519606
muhamad.rifqi510@gmail.com <muhamad.rifqi510@gmail.com>
2023-09-11 20:57:33 +0700

    create html to show the name of application, my name, and my class

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.show_home, name='show_home'),
+    path('', views.show_main, name='show_home'),
+    path('home/', views.show_home, name='show_home'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,6 +4,15 @@ from django.template import loader
 from .models import Item
 
 # Create your views here.
+def show_main (request):
+  template = loader.get_template('main.html')
+  context = {
+    "nama":"Muhamad Rifqi",
+    "kelas":"PBP F",
+    "nama_aplikasi":"tokopakedi"
+  }
+  return HttpResponse(template.render(context, request))
+
 def show_home (request):
   template = loader.get_template('home.html')
   context = {
