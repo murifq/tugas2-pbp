154145b6421c34f8400fe4330da3add7e4053d01
muhamad.rifqi510@gmail.com <muhamad.rifqi510@gmail.com>
2023-11-22 01:05:07 +0700

    redeploy

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,39 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.contrib.auth import logout as auth_logout
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as auth_login
 
 # Create your views here.
+
+@csrf_exempt
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            auth_login(request, user)
+            # Status login sukses.
+            return JsonResponse({
+                "username": user.username,
+                "status": True,
+                "message": "Login sukses!"
+                # Tambahkan data lainnya jika ingin mengirim data ke Flutter.
+            }, status=200)
+        else:
+            return JsonResponse({
+                "status": False,
+                "message": "Login gagal, akun dinonaktifkan."
+            }, status=401)
+
+    else:
+        return JsonResponse({
+            "status": False,
+            "message": "Login gagal, periksa kembali email atau kata sandi."
+        }, status=401)
+
 @csrf_exempt
 def logout(request):
     username = request.user.username
