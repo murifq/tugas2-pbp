8c7410db08e1892933d7a68e19a7165967930820
muhamad.rifqi510@gmail.com <muhamad.rifqi510@gmail.com>
2023-11-22 09:04:21 +0700

    change csrf setting

--- tokopakedi/settings.py
+++ tokopakedi/settings.py
@@ -153,3 +153,5 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+
+CSRF_TRUSTED_ORIGINS =["https://muhamad-rifqi21-tugas.pbp.cs.ui.ac.id"]
